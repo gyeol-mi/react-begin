62e9fc60de04b922fb407c80319808fe02e1261e
emily kang [강경임] <strongwomankky@gmail.com>
2025-03-10 03:29:13 +0900

    #3.2_routes

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -25,7 +25,9 @@ function Home() {
         <div>
           {movies.map((movie) => (
             <Movie
+              key={movie.id}
               coverImg={movie.medium_cover_image}
+              id={movie.id}
               title={movie.title}
               summary={movie.summary}
               genres={movie.genres}
